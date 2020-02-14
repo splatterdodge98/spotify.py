a56d328a913681d7f718cca99778c4ef3d915d5c
Alexander-Brems <abrems13@yahoo.com>
2020-02-14 11:17:50 -0700

    Some short edits and comments.

--- objects.py
+++ objects.py
@@ -7,7 +7,7 @@ class album:
         self.externalIds = externalIds  # ExternalID Object
         self.externalUrls = externalUrls  # ExternalURLS Object
         self.genres = genres  # list of strings
-        self.id = album_id  # String
+        self.album_id = album_id  # String
         self.images = images  # Url for the image
         self.name = name  # String
         self.releaseDate = releaseDate  # String that we'll convert to DateTime
@@ -19,12 +19,12 @@ class RecommendationSeedObject:
 
     def __init__(self, initialPoolSize, afterFilterSize, afterRelinkingSize, href, recomendation_id,
                  recomendation_type):
-        self.init_pool_size = initialPoolSize
-        self.after_filter_size = afterFilterSize
-        self.after_relegation_size = afterRelinkingSize
-        self.href = href
-        self.recomendation_id = recomendation_id
-        self.recomendation_type = recomendation_type
+        self.init_pool_size = initialPoolSize #Integer
+        self.after_filter_size = afterFilterSize #Integer
+        self.after_relegation_size = afterRelinkingSize #Integer ()
+        self.href = href #String
+        self.recomendation_id = recomendation_id #String
+        self.recomendation_type = recomendation_type #String
 
 
 class ContextObject:
