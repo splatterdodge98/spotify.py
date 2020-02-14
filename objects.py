e1da71e8feb3b4e1169d4bc004c24d0d11cbadba
Alexander-Brems <abrems13@yahoo.com>
2020-02-14 11:27:46 -0700

    More Edits. Yaaay.

--- objects.py
+++ objects.py
@@ -21,7 +21,7 @@ class RecommendationSeedObject:
                  recomendation_type):
         self.init_pool_size = initialPoolSize #Integer
         self.after_filter_size = afterFilterSize #Integer
-        self.after_relegation_size = afterRelinkingSize #Integer ()
+        self.after_relegation_size = afterRelinkingSize #Integer
         self.href = href #String
         self.recomendation_id = recomendation_id #String
         self.recomendation_type = recomendation_type #String
@@ -29,16 +29,16 @@ class RecommendationSeedObject:
 
 class ContextObject:
     def __init__(self, uri, href, external_urls, context_type, ):
-        self.uri = uri
-        self.href = href
-        self.external_urls = external_urls
-        self.context_type = context_type
+        self.uri = uri #String
+        self.href = href #String
+        self.external_urls = external_urls #String Dictionary of Size 2 (External URL Object)
+        self.context_type = context_type #String
 
 
 class CursorBasedPagingObject:
     def __init__(self, href, items, limit, next_page, cursors, total, ):
-        self.href = href
-        self.items = items
-        self.next_page = next_page
-        self.cursors = cursors
-        self.total = total
+        self.href = href #String
+        self.items = items #Array of Objects (Unsure what kind)
+        self.next_page = next_page #String (URL of next page)
+        self.cursors = cursors #String (Cursor Object)
+        self.total = total #Integer
