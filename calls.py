78f5af37189480a67efa87382c77b78c9baee6a6
Alex-Brems <abrems13@yahoo.com>
2020-03-29 18:22:31 -0600

    Alex also did a thing yay.
    Except in reality he had Noah helped him do the thing.

--- calls.py
+++ calls.py
@@ -1,6 +1,5 @@
 import requests
 import objects
-import datetime
 
 def getAlbum(refresh_token, client_id, client_secret, id):
     new_access_token = requests.post('https://accounts.spotify.com/api/token', data = {'grant_type':'refresh_token', 'refresh_token':refresh_token, 'client_id':client_id, 'client_secret':client_secret})
@@ -18,4 +17,18 @@ def getAlbum(refresh_token, client_id, client_secret, id):
         list_of_images.append(images['url'])
 
     return_obj = objects.Album(album["album_type"], list_of_artists, album['external_ids'], album['external_urls'], album['genres'], album['id'], list_of_images, album['name'], album['release_date'], list_of_tracks, album['uri'])
+    return return_obj
+
+def getArtist(refresh_token, client_id, client_secret, artist_id):
+    new_access_token = requests.post('https://accounts.spotify.com/api/token',
+                                     data={'grant_type': 'refresh_token', 'refresh_token': refresh_token,
+                                           'client_id': client_id, 'client_secret': client_secret})
+    access_token = new_access_token.json()['access_token']
+    artist_temp = requests.get("https://api.spotify.com/v1/artists/%s" % artist_id,
+                              headers={'Authorization': 'Bearer ' + access_token})
+    artist = artist_temp.json()
+    list_of_images = []
+    for images in artist["images"]:
+        list_of_images.append(images["url"])
+    return_obj = objects.Artist(artist["external_urls"], artist["followers"]["total"], artist["genres"], artist["href"], artist["id"], list_of_images, artist["name"], artist["popularity"], artist["type"], artist["uri"])
     return return_obj
