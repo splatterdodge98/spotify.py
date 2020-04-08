41cd925940e08383704f7a64e046e3aa21b87f16
Alex-Brems <abrems13@yahoo.com>
2020-04-08 14:50:59 -0600

    Added getManyArtists
    It doesn't work yet but I'm getting there, I swear

--- calls.py
+++ calls.py
@@ -19,6 +19,10 @@ def getAlbum(refresh_token, client_id, client_secret, id):
     return_obj = objects.Album(album["album_type"], list_of_artists, album['external_ids'], album['external_urls'], album['genres'], album['id'], list_of_images, album['name'], album['release_date'], list_of_tracks, album['uri'])
     return return_obj
 
+##############
+# Artist API #
+##############
+
 def getArtist(refresh_token, client_id, client_secret, artist_id):
     new_access_token = requests.post('https://accounts.spotify.com/api/token',
                                      data={'grant_type': 'refresh_token', 'refresh_token': refresh_token,
@@ -32,3 +36,20 @@ def getArtist(refresh_token, client_id, client_secret, artist_id):
         list_of_images.append(images["url"])
     return_obj = objects.Artist(artist["external_urls"], artist["followers"]["total"], artist["genres"], artist["href"], artist["id"], list_of_images, artist["name"], artist["popularity"], artist["type"], artist["uri"])
     return return_obj
+
+def getManyArtists(refresh_token, client_id, client_secret, list_of_ids):
+    new_access_token = requests.post('https://accounts.spotify.com/api/token',
+                                     data={'grant_type': 'refresh_token', 'refresh_token': refresh_token,
+                                           'client_id': client_id, 'client_secret': client_secret})
+    access_token = new_access_token.json()['access_token']
+    starting_list = requests.get("https://api.spotify.com/v1/%s" % list_of_ids,
+                              headers={'Authorization': 'Bearer ' + access_token})
+    real_list = starting_list.json()
+    actual_ids = list_of_ids.split(',', 49)
+    id_loc = 0
+    returned_list = {}
+    for artist in real_list["artists"]:
+        name = artist["name"]
+        returned_list[name] = actual_ids[id_loc]
+        id_loc += 1
+    return returned_list
